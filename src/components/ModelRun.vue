d7c0ca86bb336a79ac004666a537d3af25f46600
Nick Santos <ns-ffc_api_client@maskedmail.net>
2020-08-28 11:04:28 -0700

    Refactored results plot into its own component

--- src/components/ModelRun.vue
+++ src/components/ModelRun.vue
@@ -31,37 +31,22 @@
 
               </p>
             <Plotly :data="modification_scatter_data"></Plotly>
-
-          <v-autocomplete
-              v-model="selected_years"
-              :items="years"
-              clearable
-              deletable-chips
-              chips
-              small-chips
-              label="Years"
-              return-object
-              persisten-hint
-              multiple
-              solo
-          ></v-autocomplete>
-            <Plotly :data="basic_result_scatter_data" :layout="basic_result_scatter_layout"></Plotly>
+            <ResultsVisualizer :model_data="waterspout_data"></ResultsVisualizer>
         </v-flex>
-        <vflex xs9 id="model_run_container" v-if="!model_loaded">
+        <v-flex xs9 id="model_run_container" class="loading" v-if="!model_loaded">
           Loading...
-        </vflex>
+        </v-flex>
     </v-layout>
 </template>
 
 <script>
-    import { Plotly } from 'vue-plotly';
+    import { Plotly } from 'vue-plotly'
+    import ResultsVisualizer from "@/components/ResultsVisualizer"
     export default {
         name: "ModelRun",
-        components: { Plotly },
+        components: { Plotly, ResultsVisualizer },
         data: function() {
             return {
-                years: [],
-                selected_years: [],
                 model_loaded: false, // this will be false, then we'll set it to true once we're done loading everything
                 waterspout_data: {"region_modifications": []},
                 modifications_headers: [
@@ -81,7 +66,6 @@
                               .then(function(model_run){
                                 vm.waterspout_data = model_run;
                                 console.log(model_run);
-                                vm.set_up_component();
                                 vm.model_loaded = true;
                               });
 
@@ -93,14 +77,6 @@
         //  next();
        // },
         methods:{
-            set_up_component: function(){
-              if(this.has_results){ // if we have results
-                console.log("Calculating years")
-                // get the distinct set of years that the model results cover
-                this.years = [...new Set(this.waterspout_data.results.result_set.map(result => result.year))];
-                this.selected_years = this.years;
-              }
-            },
             get_region_name_by_id: function(id){
                 return this.$store.state.regions.find(r => Number(r.id) === Number(id)).name
             }
@@ -128,33 +104,6 @@
                 mode: "markers",
                 type: "scatter"
               }]
-            },
-            basic_result_scatter_data: function(){
-              let x = [];
-              let y = [];
-
-              this.waterspout_data.results.result_set
-                  .filter(result => this.selected_years.includes(result.year))  // filter by year first
-                  .forEach(function(result){
-                      x.push(result.xlandsc);
-                      y.push(result.xwatersc);
-              });
-              return [{
-                x: x,
-                y: y,
-                mode: "markers",
-                type: "scatter"
-              }]
-            },
-            basic_result_scatter_layout: function(){
-              return {
-                xaxis: {
-                  title:'Land'
-                },
-                yaxis: {
-                  title:'Water'
-                }
-              }
             }
         }
     }
@@ -167,6 +116,9 @@
   #model_run_container
     margin-left: auto;
     margin-right: auto;
+
+    .loading
+      text-align: center;
 
   button.v-btn.primary
     a
---
+++ src/components/ResultsVisualizer.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-flex row class="results_visualizer">
+    <v-autocomplete
+        v-model="selected_years"
+        :items="years"
+        clearable
+        deletable-chips
+        chips
+        small-chips
+        label="Years"
+        return-object
+        persisten-hint
+        multiple
+        solo
+    ></v-autocomplete>
+    <Plotly :data="basic_result_scatter_data" :layout="basic_result_scatter_layout"></Plotly>
+  </v-flex>
+</template>
+
+<script>
+import { Plotly } from 'vue-plotly'
+
+export default {
+  name: "ResultsVisualizer",
+  components: { Plotly },
+  props:{
+    model_data: Object
+  },
+  data: function(){
+    return {
+      years: [],
+      selected_years: []
+    }
+  },
+  watch: {
+    model_data: {
+      immediate: true,
+      handler: function() {
+        this.set_up_component();
+      }
+    }
+  },
+  methods: {
+    set_up_component: function () {
+      if (this.has_results) { // if we have results
+        console.log("Calculating years")
+        // get the distinct set of years that the model results cover
+        this.years = [...new Set(this.model_data.results.result_set.map(result => result.year))];
+        this.selected_years = this.years;
+      }else{
+        console.log("Skipping Year Calculation")
+      }
+    }
+  },
+  computed: {
+    has_results: function(){
+      return this.model_data.results !== undefined && this.model_data.results !== null;
+    },
+    basic_result_scatter_data: function(){
+      let x = [];
+      let y = [];
+
+      this.model_data.results.result_set
+          .filter(result => this.selected_years.includes(result.year))  // filter by year first
+          .forEach(function(result){
+            x.push(result.xlandsc);
+            y.push(result.xwatersc);
+          });
+      return [{
+        x: x,
+        y: y,
+        mode: "markers",
+        type: "scatter"
+      }]
+    },
+    basic_result_scatter_layout: function(){
+      return {
+        xaxis: {
+          title:'Land'
+        },
+        yaxis: {
+          title:'Water'
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+.plot-container.plotly
+  min-height: 500px;
+</style>
