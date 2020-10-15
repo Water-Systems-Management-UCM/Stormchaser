c400e30cacac31422d26689487b240c56cd73958
Nick Santos <ns-ffc_api_client@maskedmail.net>
2020-10-15 16:16:39 -0700

    Adding Sentry to Stormchaser

--- src/main.js
+++ src/main.js
@@ -14,6 +14,28 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css' // need t
 
 import vuetify from '@/plugins/vuetify' // path to vuetify export
 
+// Set up Sentry first
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://a39545e9b4c940c18e62629856dedaed@o462396.ingest.sentry.io/5465746",
+  integrations: [
+    new VueIntegration({
+      Vue,
+      tracing: true,
+      logErrors: true // make sure that errors still show in the console
+    }),
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+// Now init the application itself
 Vue.use(VueRouter)
 Vue.config.productionTip = false
 
