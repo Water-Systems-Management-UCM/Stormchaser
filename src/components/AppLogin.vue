ad47d8b245da7f83c9f60a111512d8d1118eccc2
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2020-11-18 13:08:03 -0800

    Made visual change to the login page

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -1,74 +1,83 @@
 <template>
-  <v-flex class="login">
-    <notification-snackbar
-      v-model="login_failed_snackbar"
-      constant_snackbar_text="Failed to log you in"
-      :error_text="login_failed_text"
-    ></notification-snackbar>
-    <h2>Login</h2>
+  <div class="login-container">
+    <br />
+    <br />
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6" id="middle-col">
+        <v-flex class="login">
+          <notification-snackbar
+            v-model="login_failed_snackbar"
+            constant_snackbar_text="Failed to log you in"
+            :error_text="login_failed_text"
+          ></notification-snackbar>
+          <h1 id="login-text">Login</h1>
 
-    <v-form @submit.prevent="do_login">
-      <v-text-field
-        v-model="username"
-        label="Username"
-        required
-        :rules="username_rules"></v-text-field>
-      <v-text-field
-        v-model="password"
-        label="Password"
-        type="password"
-        required
-        :rules="password_rules"></v-text-field>
+          <v-form @submit.prevent="do_login">
+            <v-text-field
+              v-model="username"
+              label="Username"
+              required
+              :rules="username_rules"
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="Password"
+              type="password"
+              required
+              :rules="password_rules"
+            ></v-text-field>
 
-      <v-btn
-        type="submit"
-        :disabled="!form_valid"
-      >Log In</v-btn>
-    </v-form>
-  </v-flex>
+            <v-btn type="submit" :disabled="!form_valid">Log In</v-btn>
+          </v-form>
+        </v-flex>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>
 </template>
 
 <script>
 import NotificationSnackbar from "@/components/NotificationSnackbar";
 export default {
   name: "AppLogin",
-  components: {NotificationSnackbar},
-  data: function(){
+  components: { NotificationSnackbar },
+  data: function () {
     return {
       username: null,
       password: null,
       login_failed_snackbar: false,
       login_failed_text: "",
       username_rules: [
-        v => !!v || "Username is required",
-          v => v.length >= 3 || "Invalid username"
+        (v) => !!v || "Username is required",
+        (v) => v.length >= 3 || "Invalid username",
       ],
-      password_rules: [
-        v => !!v || "Password is required",
-      ]
-    }
+      password_rules: [(v) => !!v || "Password is required"],
+    };
   },
   computed: {
-    form_valid: function(){
+    form_valid: function () {
       return this.username && this.password;
-    }
+    },
   },
   methods: {
-    do_login(){
+    do_login() {
       let login_promise = this.$store.dispatch("do_login", {
-        "username": this.username,
-        "password": this.password
-      })
-
-      login_promise.then((response) => {
-        if ("non_field_errors" in response){
-          this.login_failed_text = response.non_field_errors[0];  // show just the first item - it'll be a list, but let's just tell them one by one right now
-          this.login_failed_snackbar = true;
-        }
-      }).catch(() => {
-        this.login_failed_text = "Unknown login error"
-        this.login_failed_snackbar = true;
+        username: this.username,
+        password: this.password,
       });
+
+      login_promise
+        .then((response) => {
+          if ("non_field_errors" in response) {
+            this.login_failed_text = response.non_field_errors[0]; // show just the first item - it'll be a list, but let's just tell them one by one right now
+            this.login_failed_snackbar = true;
+          }
+        })
+        .catch(() => {
+          this.login_failed_text = "Unknown login error";
+          this.login_failed_snackbar = true;
+        });
 
       // try the login
 
@@ -78,11 +87,20 @@ export default {
       // If it doesn't work, then handle the error by reading the JSON and setting the snack bar error message
 
       // If it doesn't work and we can't read the JSON, note that an unknown error occurred (and an admin is notified??)
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
+#middle-col {
+  margin-top: 10%;
+  background-color: white;
+  padding: 2%;
+  border-radius: 10xpx;
+}
 
+#login-text {
+  color: grey;
+}
 </style>
