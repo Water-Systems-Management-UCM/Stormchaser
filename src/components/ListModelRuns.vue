d6322fa23ca59b9320faa230d8b0282dabe6f683
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2020-11-18 18:01:51 -0800

    made delete button with confirmation modal

--- src/components/ListModelRuns.vue
+++ src/components/ListModelRuns.vue
@@ -1,69 +1,97 @@
 <template>
-    <v-flex>
-        <v-flex style="margin:auto">
-            <h2>Model Runs</h2>
-              <v-btn v-on:click="refresh_model_runs">
-                <v-icon>mdi-refresh</v-icon> Update
-              </v-btn>
-            <!--<ul>
+  <v-flex>
+    <v-flex style="margin: auto">
+      <h2>Model Runs</h2>
+      <div class="row" style="padding: 1%">
+        <div class="col-md-1" id="left-col">
+          <v-btn @click="refresh_model_runs">
+            <v-icon>mdi-refresh</v-icon> Update
+          </v-btn>
+        </div>
+        <div class="col-md-1" id="left-col">
+          <v-btn dark v-bind="attrs" @click="confirmDeleteDialog=true">
+            <v-icon>mdi-delete</v-icon> Delete
+          </v-btn>
+          <v-dialog v-model="confirmDeleteDialog" persistent max-width="30%">
+            <v-card>
+              <v-card-title class="headline"> Delete model runs? </v-card-title>
+              <v-card-text
+                >Would you like to delete the selected runs?</v-card-text
+              >
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="black" text @click="confirmDeleteDialog = false"> Cancel </v-btn>
+                <v-btn color="red" text @click="delete_model_runs"> Confirm </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+      </div>
+
+      <!--<ul>
                 <li v-for="m in $store.state.model_runs"
                 v-bind:key="m.id"
                 ><router-link :to="{name:'model-run', params:{id: m.id}}">{{ m.name }} - {{ m.date_submitted }}</router-link> </li>
             </ul>
             <p style="height:800px">&nbsp;</p>-->
-            <v-data-table
-                    v-model="selected"
-                    :headers="headers"
-                    :items="model_runs"
-                    item-key="id"
-                    show-select
-                    multi-sort
-                    sort-by="date_submitted"
-                    sort-desc
-                    class="elevation-1 model_run_listing"
-                    items-per-page="20"
-                    @click:row="view_model_run"
-            >
-              <template v-slot:item.complete="{ item }">
-                <span>{{ $stormchaser_utils.model_run_status_text(item) }}</span>
-              </template>
-            </v-data-table>
-        </v-flex>
+      <v-data-table
+        v-model="selected"
+        :headers="headers"
+        :items="model_runs"
+        item-key="id"
+        show-select
+        multi-sort
+        sort-by="date_submitted"
+        sort-desc
+        class="elevation-1 model_run_listing"
+        items-per-page="20"
+        @click:row="view_model_run"
+      >
+        <template v-slot:item.complete="{ item }">
+          <span>{{ $stormchaser_utils.model_run_status_text(item) }}</span>
+        </template>
+      </v-data-table>
     </v-flex>
+  </v-flex>
 </template>
 
 <script>
-    export default {
-        name: "ListModelRuns",
-        data: function(){
-            return {
-                headers: [
-                    {text: 'Run Name', value: 'name' },
-                    {text: 'Description', value: 'description' },
-                    {text: 'Date', value: 'date_submitted' },
-                    {text: 'Status', value: 'complete' },
-                ]
-            }
-        },
-        methods: {
-          view_model_run: function(item){
-            this.$router.push({name: "model-run", params: {id: item.id}})
-          },
-          refresh_model_runs: function(){
-            this.$store.dispatch("fetch_model_runs")
-          },
-        },
-        computed: {
-          model_runs: function(){
-            return Object.values(this.$store.getters.current_model_area.model_runs);
-          }
-        }
-    }
+export default {
+  name: "ListModelRuns",
+  data: function () {
+    return {
+      confirmDeleteDialog: false,
+      headers: [
+        { text: "Run Name", value: "name" },
+        { text: "Description", value: "description" },
+        { text: "Date", value: "date_submitted" },
+        { text: "Status", value: "complete" },
+      ],
+    };
+  },
+  methods: {
+    view_model_run: function (item) {
+      this.$router.push({ name: "model-run", params: { id: item.id } });
+    },
+    refresh_model_runs: function () {
+      this.$store.dispatch("fetch_model_runs");
+    },
+    delete_model_runs: function () {
+      this.confirmDeleteDialog = false;
+      this.$store.dispatch("delete_model_run");
+    },
+  },
+  computed: {
+    model_runs: function () {
+      return Object.values(this.$store.getters.current_model_area.model_runs);
+    },
+  },
+};
 </script>
 
 <style>
-  /* Not scoped because scoped classed incur a performance hit because of the way they use id selectors - using a class instead */
-  div.v-data-table.model_run_listing{
-    cursor: pointer;
-  }
+/* Not scoped because scoped classed incur a performance hit because of the way they use id selectors - using a class instead */
+div.v-data-table.model_run_listing {
+  cursor: pointer;
+}
 </style>
