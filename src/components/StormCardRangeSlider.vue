c8a4bc16de0c1f1766d252e4ea9d6b04f9845ebf
Nick Santos <nickrsan@users.noreply.github.com>
2021-06-18 13:58:58 -0700

    Fix for crop constraint slider not updating values

--- src/components/StormCardRangeSlider.vue
+++ src/components/StormCardRangeSlider.vue
@@ -134,36 +134,36 @@
             },
             activate_upper_limit: function(){
               this.slider_value = [this.slider_value[0], this.max] // set the whole array to trigger the watcher
-              this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
+              //this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
               this.current_max = this.max
               this.upper_limit = true
             },
             remove_upper_limit: function(){
               this.upper_limit = false
               this.slider_value = [this.slider_value[0], null]  // set the whole array to trigger the watcher
-              this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
+              //this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
               this.current_max = null
             },
             increment_lower_slider_value: function(){
                 // can't just increment/decrement - the watchers don't get updated then. Could probably do object.assign or something instead though
                 this.slider_value = [this.slider_value[0]+1, this.slider_value[1]];
-                this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
+                //this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
             },
             decrement_lower_slider_value: function(){
                 this.slider_value = [this.slider_value[0]-1, this.slider_value[1]];
-                this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
+                //this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
             },
             increment_upper_slider_value: function(){
                 this.slider_value = [this.slider_value[0], this.slider_value[1]+1];
-                this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
+                //this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
             },
             decrement_upper_slider_value: function(){
                 this.slider_value = [this.slider_value[0], this.slider_value[1]-1];
-                this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
             },
         },
         watch: {
             slider_value() {
+                this.slider_value_input = [this.slider_value[0], this.slider_value[1]];
                 this.$emit('input', this.slider_value);
                 this.$emit('userchanged');
             }
