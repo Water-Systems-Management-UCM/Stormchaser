0146f197713a988a178e08b620b484f3ab1c92a6
Nick Santos <nickrsan@users.noreply.github.com>
2021-06-28 13:19:44 -0700

    Include the Dapper version on the status card

--- src/components/ModelRun.vue
+++ src/components/ModelRun.vue
@@ -109,6 +109,11 @@
                       solo
                   ></v-autocomplete>
                 </v-row>
+                <v-row v-if="has_results">
+                  <v-col class="col-12">
+                    <p style="font-size:0.75em">Results from model version {{ results.dapper_version }}</p>
+                  </v-col>
+                </v-row>
               </v-card>
             </v-col>
             <v-col class="col-12 col-md-4">
