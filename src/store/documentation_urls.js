e29c4b2cb288664fdfc8a2ab21b24d90c3e6035d
Nick Santos <nickrsan@users.noreply.github.com>
2021-06-28 13:35:07 -0700

    Adding link to multiple results sets tooltip

--- src/store/documentation_urls.js
+++ src/store/documentation_urls.js
@@ -5,6 +5,9 @@ const DOCS = {
         'advanced_region_options': `${BASE_DOCS_URL}/MakeModelRun/region_modifications.html#advanced-region-options`,
         'automatic_crop_card_addition': `${BASE_DOCS_URL}/MakeModelRun/crop_modifications.html#automatic-addition-of-crop-modification-cards`,
         'region_linked_crop_cards': `${BASE_DOCS_URL}/MakeModelRun/crop_modifications.html#region-linked-crops`,
+    },
+    'model_runs':{
+        'multiple_results_sets': `${BASE_DOCS_URL}/ModelRuns/model_runs.html#multiple-results-sets`,
     }
 }
 
