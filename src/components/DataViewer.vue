6f670550f0036cd71d9147e6de33a7a45343c3fa
Michael Tapia <73376610+mtapia01@users.noreply.github.com>
2026-08-19 10:55:20 -0700

    Fixes makemodel constraints (#64)
    * Fix(dataviewer): Added case to show region group toggle when present
    
    # Conflicts:
    #	src/components/DataViewer.vue

--- src/components/DataViewer.vue
+++ src/components/DataViewer.vue
@@ -183,16 +183,19 @@
               ></v-switch>
 
               <h4>Show Region Breakdown</h4>
-              <v-row>
+              <v-switch
+                  v-model="charts_toggle_region"
+                  label="Show Regions Values"
+              ></v-switch>
+              <v-row v-if="$store.getters.current_model_area.region_group_sets.length > 0">
                 <v-switch
-                    v-model="charts_toggle_region"
-                    label="Show Regions Values"
+                    v-model="toggle_list_region_group"
+                    label="Show Regions Groups"
                 ></v-switch>
                 <SimpleTooltip
                     :text_only="true"> By default, region groups will be shown on the chart as one column. Enabling this
                     switch will allow to list the region group individually
                 </SimpleTooltip>
-
               </v-row>
 
             </v-col>
